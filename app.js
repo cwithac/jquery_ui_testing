42d51efeaafdee6ec516501a1e00a5bec8da27c9
Cathleen Wright <cathleenmwright@gmail.com>
2018-04-04 18:04:02 -0400

    Droppable

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 $(function() {
-  $('.box').draggable();
-  $('#box1').draggable({ containment: 'parent', scroll: false });
-  // $('#box1').draggable({ containment: ".container", scroll: false });
-  $('#box2').draggable({axis: 'x'});
-  // $('#box2').draggable({axis: 'y'});
+  //DRAGGABLE
+  // https://jqueryui.com/draggable/
+    $('.box').draggable();
+    $('#box1').draggable({ containment: 'parent', scroll: false, revert: 'invalid' });
+    // $('#box1').draggable({ containment: 'parent', scroll: false });
+    // // $('#box1').draggable({ containment: ".container", scroll: false });
+    // $('#box2').draggable({axis: 'x'});
+    // // $('#box2').draggable({axis: 'y'});
+
+  //DROPPABLE
+  // https://jqueryui.com/droppable/
+  $('#droppable').droppable({
+    accept: '#box1',
+    drop: function() {
+      $(this).find('span').html("Drop it like it's hot.")
+    }
+  });
 });
