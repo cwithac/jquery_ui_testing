8a5806615b6ae3679042bff9c2bb57a962188a21
Cathleen Wright <cathleenmwright@gmail.com>
2018-04-04 18:10:51 -0400

    Sortable

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(function() {
   //DRAGGABLE
   // https://jqueryui.com/draggable/
-    $('.box').draggable();
-    $('#box1').draggable({ containment: 'parent', scroll: false, revert: 'invalid' });
+    // $('.box').draggable();
+    // $('#box1').draggable({ containment: 'parent', scroll: false, revert: 'invalid' });
     // $('#box1').draggable({ containment: 'parent', scroll: false });
     // // $('#box1').draggable({ containment: ".container", scroll: false });
     // $('#box2').draggable({axis: 'x'});
@@ -10,10 +10,19 @@ $(function() {
 
   //DROPPABLE
   // https://jqueryui.com/droppable/
-  $('#droppable').droppable({
-    accept: '#box1',
-    drop: function() {
-      $(this).find('span').html("Drop it like it's hot.")
-    }
-  });
+  // $('#droppable').droppable({
+  //   accept: '#box1',
+  //   drop: function() {
+  //     $(this).find('span').html("Drop it like it's hot.")
+  //   }
+  // });
+
+  //SORTABLE
+  // https://jqueryui.com/sortable/
+  // $('#sortable').sortable();
+  $('#sortableToo').sortable();
+  // $('#sortable').sortable({connectWith: '#sortableToo'});
+  $('#sortable').sortable({connectWith: '#sortableToo', placeholder: 'placeholderBox'}); //placeholderBox is styled class
+  $('#sortableToo').sortable({connectWith: '#sortable'});
+
 });
